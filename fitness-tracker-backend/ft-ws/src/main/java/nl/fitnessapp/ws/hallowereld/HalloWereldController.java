928059f5860fc52ebc9f573f882ddad9c22998c5
DavidRouw17 <david_rouw@hotmail.com>
2022-07-22 14:07:28 +0200

    dto en ws-api worden nu gegenereerd

--- fitness-tracker-backend/ft-ws/src/main/java/nl/fitnessapp/ws/hallowereld/HalloWereldController.java
+++ fitness-tracker-backend/ft-ws/src/main/java/nl/fitnessapp/ws/hallowereld/HalloWereldController.java
@@ -1,15 +1,21 @@
 package nl.fitnessapp.ws.hallowereld;
 
+import fitnesstracker.api.HallowereldApi;
+import fitnesstracker.model.HalloWereld;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "hallowereld")
-public class HalloWereldController {
+public class HalloWereldController implements HallowereldApi {
 
-    @GetMapping
-    public String halloWereld(){
-        return "Ik leef!";
+
+    @Override
+    public ResponseEntity<HalloWereld> geefHalloTerug() {
+       HalloWereld halloWereld = new HalloWereld();
+       halloWereld.setBericht("Ik leef!");
+
+       return ResponseEntity.ok(halloWereld);
     }
 }
